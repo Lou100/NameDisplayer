1f6ece71a9d3db2e535a4185059552b1c8d6800e
Lou100 <loulouangibeau@yahoo.com>
2020-02-22 03:02:18 -0500

    Initial commit

---
+++ src/com/Main/HashMaps/Main.java
@@ -0,0 +1,17 @@
+package com.Main.HashMaps;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a name: ");
+        var n = reader.next(); // Scans the next token of the input and saves it as a variable n.
+//once finished
+        System.out.println("Hi" + " " + n);// Prints Hi and adds in the saved information in variable n.
+
+
+    }
+}
